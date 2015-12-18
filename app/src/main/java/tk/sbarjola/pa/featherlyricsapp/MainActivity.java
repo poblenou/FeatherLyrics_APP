108dcb203c5bfe1b53d905cfd2962daf60cc40de
Sergi Barjola <sergibca@gmail.com>
2015-12-18 20:14:43 +0100

    Implementado el Navigation Drawer con fragments.
    Creados los fragments que llamaremos.

--- app/src/main/java/tk/sbarjola/pa/featherlyricsapp/MainActivity.java
+++ app/src/main/java/tk/sbarjola/pa/featherlyricsapp/MainActivity.java
@@ -1,9 +1,7 @@
 package tk.sbarjola.pa.featherlyricsapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,16 +11,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import tk.sbarjola.pa.featherlyricsapp.Fragments.Artistas;
+import tk.sbarjola.pa.featherlyricsapp.Fragments.Discografia;
+import tk.sbarjola.pa.featherlyricsapp.Fragments.Home;
+import tk.sbarjola.pa.featherlyricsapp.Fragments.Noticias;
+import tk.sbarjola.pa.featherlyricsapp.Fragments.RankingArtistas;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,16 +49,7 @@ public class MainActivity extends AppCompatActivity
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        });*/
     }
 
     @Override
@@ -52,6 +62,48 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        boolean transaccion = false;
+
+        if (id == R.id.nav_home) {
+            fragment = new Home();
+            transaccion = true;
+        }
+        else if (id == R.id.nav_artistas) {
+            fragment = new Artistas();
+            transaccion = true;
+        }else if (id == R.id.nav_discografia) {
+            fragment = new Discografia();
+            transaccion = true;
+        } else if (id == R.id.nav_noticias) {
+            fragment = new Noticias();
+            transaccion = true;
+        } else if (id == R.id.nav_rankingArtistas) {
+            fragment = new RankingArtistas();
+            transaccion = true;
+        } else if (id == R.id.nav_salir) {
+            finish();
+        }
+
+        if(transaccion){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+
+        }
+
+        drawer.closeDrawers();
+        return true;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,42 +111,17 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_artistas) {
-            //fragment = new Fragment1();
-            //fragmentTransaction = true;
-        } else if (id == R.id.nav_discografia) {
-            // Intents
-        } else if (id == R.id.nav_noticias) {
-            // Intents
-        } else if (id == R.id.nav_rankingArtistas) {
-            // Intents
-        } else if (id == R.id.nav_salir) {
-            finish();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
